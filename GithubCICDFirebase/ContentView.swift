7ccde4a596f94c8af23b07d72eebf69420cae1ab
chhan-sophearith <chhansophearithit@gmail.com>
2024-07-12 14:54:19 +0700

    dng. nden

--- GithubCICDFirebase/ContentView.swift
+++ GithubCICDFirebase/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worlfewfwgvr3wvrwd!")
+            Text("Hello, worlfed!")
         }
         .padding()
     }
