0b698dd10244dce5ee535816d49cc8d573a49171
chhan-sophearith <chhansophearithit@gmail.com>
2024-07-12 09:30:04 +0700

    init project

---
+++ GithubCICDFirebase/GithubCICDFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubCICDFirebaseApp.swift
+//  GithubCICDFirebase
+//
+//  Created by Brilliant Dev on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubCICDFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
